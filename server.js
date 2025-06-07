30db7b39d2081b7a96a25df71ab2656343aed407
RajG98 <rajarshig007@gmail.com>
2025-06-07 18:19:21 +0530

    update 07.06.25

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(8081, () => {
-    console.log('Server is running on http://localhost:8081');
+    console.log('Server is running!');
 });
 
 // GET all users
